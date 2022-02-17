fddae2cec5f2ba0fdf6a0b421c03d9f49ca663fc
kavyapandey <pandeykavya2@gmail.com>
2022-02-17 18:08:00 +0530

    cloudinary changes

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ const express = require("express");
 const app = express();
 const cors = require ('cors');
 const dotenv = require("dotenv");
+const { cloudinary } = require('./utils/cloudinary');
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-const multer = require("multer");
-const path = require("path");
+
 
 dotenv.config();
 app.use(cors({
   origin : "*"
 }))
 
-app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
+
+app.use(express.static('public'));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true}));
+
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -26,18 +29,20 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
+app.post('/api/upload', async (req, res) => {
+    try {
+        const fileData = req.body.data;
+        const uploadResponse = await cloudinary.uploader.upload(fileData, {
+            upload_preset: 'ml_default',
+        });
+        console.log(uploadResponse);
+        res.status(200).json({photo:uploadResponse.url})
+        // res.json({ msg: 'successfully uploaded' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: 'Something went wrong' });
+    }
 });
 
 app.use("/api/auth", authRoute);
